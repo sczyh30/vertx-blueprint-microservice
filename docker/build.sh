8a7077481431a5313c3e047fa01ee90be3848257
crazyfrozenpenguin <crazy.frozen.penguin@gmail.com>
2016-11-02 22:17:48 -0400

    Should build from any directory
    Introduced modifications that allow script to be executed from any directory.

--- docker/build.sh
+++ docker/build.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-docker build -t "vertx-blueprint/api-gateway" ../api-gateway
-docker build -t "vertx-blueprint/cache-infrastructure" ../cache-infrastructure
-docker build -t "vertx-blueprint/inventory-microservice" ../inventory-microservice
-docker build -t "vertx-blueprint/monitor-dashboard" ../monitor-dashboard
-docker build -t "vertx-blueprint/order-microservice" ../order-microservice
-docker build -t "vertx-blueprint/product-microservice" ../product-microservice
-docker build -t "vertx-blueprint/shopping-cart-microservice" ../shopping-cart-microservice
-docker build -t "vertx-blueprint/store-microservice" ../store-microservice
-docker build -t "vertx-blueprint/account-microservice" ../account-microservice
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker build -t "vertx-blueprint/api-gateway" $DIR/../api-gateway
+docker build -t "vertx-blueprint/cache-infrastructure" $DIR/../cache-infrastructure
+docker build -t "vertx-blueprint/inventory-microservice" $DIR/../inventory-microservice
+docker build -t "vertx-blueprint/monitor-dashboard" $DIR/../monitor-dashboard
+docker build -t "vertx-blueprint/order-microservice" $DIR/../order-microservice
+docker build -t "vertx-blueprint/product-microservice" $DIR/../product-microservice
+docker build -t "vertx-blueprint/shopping-cart-microservice" $DIR/../shopping-cart-microservice
+docker build -t "vertx-blueprint/store-microservice" $DIR/../store-microservice
+docker build -t "vertx-blueprint/account-microservice" $DIR/../account-microservice
